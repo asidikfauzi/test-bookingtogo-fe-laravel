db90ded3231918f3c09b84c3fc0b31eeb2936669
asidikfauzi <asidikfauzi@gmail.com>
2023-04-08 00:52:40 +0700

    router nationalities

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NationalitiesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,19 @@ Route::get('/layouts', function () {
     return view('layouts.app');
 });
 
+
+
+Route::prefix('booking-to-go')->group(function () {
+
+    Route::prefix('nationality')->group(function () {
+        Route::get('', [NationalitiesController::class, 'index'])->name('getNationality');
+        Route::get('/get-data', [NationalitiesController::class, 'getDataNationalities'])->name('getDataNationalities');
+        Route::get('/create', [NationalitiesController::class, 'create'])->name('createNationality');
+        Route::post('/create', [NationalitiesController::class, 'store'])->name('createNationality');
+        Route::get('/edit/{id}', [NationalitiesController::class, 'edit'])->name('updateNationality');
+        Route::put('/edit/{id}', [NationalitiesController::class, 'update'])->name('updateNationality');
+        Route::delete('/delete/{id}', [NationalitiesController::class, 'destroy'])->name('deleteNationality');
+    });
+
+
+});
