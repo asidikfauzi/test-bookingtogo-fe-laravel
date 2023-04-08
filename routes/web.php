7ce19af9070fbd40ea722105b5317d9018e98735
asidikfauzi <asidikfauzi@gmail.com>
2023-04-08 08:28:31 +0700

    router

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NationalitiesController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\FamilyListController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,6 +36,27 @@ Route::prefix('booking-to-go')->group(function () {
         Route::get('/edit/{id}', [NationalitiesController::class, 'edit'])->name('updateNationality');
         Route::put('/edit/{id}', [NationalitiesController::class, 'update'])->name('updateNationality');
         Route::delete('/delete/{id}', [NationalitiesController::class, 'destroy'])->name('deleteNationality');
+    });
+
+    Route::prefix('customer')->group(function () {
+        Route::get('', [CustomerController::class, 'index'])->name('getCustomer');
+        Route::get('/get-data', [CustomerController::class, 'getDataCustomer'])->name('getDataCustomer');
+        Route::get('/create', [CustomerController::class, 'create'])->name('createCustomer');
+        Route::post('/create', [CustomerController::class, 'store'])->name('createCustomer');
+        Route::get('/edit/{id}', [CustomerController::class, 'edit'])->name('updateCustomer');
+        Route::put('/edit/{id}', [CustomerController::class, 'update'])->name('updateCustomer');
+        Route::delete('/delete/{id}', [CustomerController::class, 'destroy'])->name('deleteCustomer');
+    });
+
+    Route::prefix('family-list')->group(function () {
+        Route::get('', [FamilyListController::class, 'index'])->name('getFamilyList');
+        Route::get('/get-data', [FamilyListController::class, 'getDataFamilyList'])->name('getDataFamilyList');
+        Route::get('/{id}', [FamilyListController::class, 'show'])->name('showFamilyList');
+        Route::get('/create', [FamilyListController::class, 'create'])->name('createFamilyList');
+        Route::post('/create', [FamilyListController::class, 'store'])->name('createFamilyList');
+        Route::get('/edit/{id}', [FamilyListController::class, 'edit'])->name('updateFamilyList');
+        Route::put('/edit/{id}', [FamilyListController::class, 'update'])->name('updateFamilyList');
+        Route::delete('/delete/{id}', [FamilyListController::class, 'destroy'])->name('deleteFamilyList');
     });
 
 
